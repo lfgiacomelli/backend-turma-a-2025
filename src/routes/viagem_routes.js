2f8391776ca41aa2d8b9f5d5e83e5818a0fd4f89
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-05-27 02:45:07 +0000

    [fix] GetViagem filter

--- src/routes/viagem_routes.js
+++ src/routes/viagem_routes.js
@@ -7,4 +7,5 @@ router.post('/', ViagemController.createViagem);
 router.patch('/:id', ViagemController.updateViagem); 
 router.delete('/:id', ViagemController.deleteViagem);  
 router.get('/:id', ViagemController.getViagemPorUsuario);
+
 export default router;
