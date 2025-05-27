99666b5f572a46ea77f9117a068ac656a91308c0
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-05-27 02:59:44 +0000

    [fix] SSL Database

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,13 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
   user: 'smithgg415',
-  host: 'dpg-d0kgkoruibrs739hd8f0-a.oregon-postgres.render.com',   
+  host: 'dpg-d0kgkoruibrs739hd8f0-a.oregon-postgres.render.com',
   database: 'zoomx_tcc',
   password: 'Jtn5fpob64g18cD9hlsZ6cXHPtoK6jTd',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 export default pool;
