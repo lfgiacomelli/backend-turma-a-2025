c61676e7e75431c8b23ca15a767770aecfb8cd89
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-05-28 17:49:10 +0000

    [fix] Alterações na função getViagens

--- src/routes/viagem_routes.js
+++ src/routes/viagem_routes.js
@@ -2,17 +2,11 @@ import express from 'express';
 import ViagemController from '../controllers/ViagemController.js';
 
 const router = express.Router();
+
 router.get('/:id', ViagemController.getViagemPorUsuario);
+
 router.get('/solicitacao/:solicitacaoId/funcionario', ViagemController.getFuncionarioPorViagem);
-router.get('/:via_codigo', ViagemController.getViagemPorId);
+
 router.get('/andamento/:id', ViagemController.verificarAndamento);
-router.get('/usuario/:usu_codigo', ViagemController.getViagensPorUsuario);
 
 export default router;
-
-
-
-
-
-
-
