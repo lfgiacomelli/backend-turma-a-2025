d401e44600ae083d0b55e0ca35cddc226dd4faa4
Giacomelli <133823261+smithgg415@users.noreply.github.com>
2025-06-08 15:23:26 +0000

    [fix] Remove authMiddleware from anuncios

--- src/routes/anuncio_routes.js
+++ src/routes/anuncio_routes.js
@@ -1,9 +1,7 @@
 import express from 'express';
 const router = express.Router();
-import authMiddleware from '../middlewares/authMiddleware.js';
-
 import AnuncioController from '../controllers/AnuncioController.js';
 
-router.get('/', authMiddleware, AnuncioController.listarTodos);
+router.get('/', AnuncioController.listarTodos);
 
 export default router;
