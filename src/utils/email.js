0fa798c1e9f8c01a25eafe0c7c2f2fdc571ea348
lfgiacomelli <lfgiacomellirodrigues@gmail.com>
2025-11-08 18:06:14 -0300

    [fix] library resend documentation

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,21 +1,24 @@
-import { Resend } from "resend";
-import dotenv from "dotenv";
+// email.js
+import { Resend } from 'resend';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function enviarEmail({ to, subject, text, html }) {
-  try {
-    await resend.emails.send({
-      from: "ZoomX <no-reply@zoomx.app>",
-      to,
-      subject,
-      text,
-      html,
-    });
-    console.log("✅ E-mail enviado com sucesso");
-  } catch (error) {
-    console.error("❌ Erro ao enviar e-mail:", error);
+  const { data, error } = await resend.emails.send({
+    from: 'ZoomX <no-reply@resend.dev>',
+    to,
+    subject,
+    text,
+    html,
+  });
+
+  if (error) {
+    console.error("❌ Erro ao enviar email:", error);
+    throw error;
   }
+
+  console.log("✅ E-mail enviado:", data);
 }
