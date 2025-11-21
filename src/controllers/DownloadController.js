b922ea5b4c5742425e019e6df2946882d01b96ce
lfgiacomelli <lfgiacomellirodrigues@gmail.com>
2025-11-21 02:31:13 -0300

    defined require to path

--- src/controllers/DownloadController.js
+++ src/controllers/DownloadController.js
@@ -1,15 +1,20 @@
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 class DownloadController {
   downloadApp(req, res) {
     try {
       const filePath = path.resolve(__dirname, '../../uploads/zoomx.apk');
+      console.log("Enviando arquivo:", filePath);
       return res.download(filePath, 'ZoomX.apk');
     } catch (error) {
-      console.error('Erro ao baixar APK:', error);
+      console.error(' Erro ao baixar APK:', error);
       return res.status(500).json({ erro: 'Erro ao baixar o arquivo' });
     }
   }
 }
 
-export default DownloadController;
+export default new DownloadController();
