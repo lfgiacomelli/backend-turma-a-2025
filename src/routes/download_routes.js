efbfc9bf52be459e63097eb9f42debb6e06b089d
lfgiacomelli <lfgiacomellirodrigues@gmail.com>
2025-11-21 02:19:46 -0300

    [fix] path to downloadcontroller without extension .js

--- src/routes/download_routes.js
+++ src/routes/download_routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import DownloadController from '../controllers/DownloadController';
+import DownloadController from '../controllers/DownloadController.js';
 
 const router = express.Router();
 
